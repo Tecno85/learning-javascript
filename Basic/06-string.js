4fe35fe516da431a80bb51f19bfa2ca212c4cbdd
Ivan Dario Madrid Daza <tecno85@gmail.com>
2025-03-25 21:10:50 -0500

    String 25-03-2025

--- Basic/06-string.js
+++ Basic/06-string.js
@@ -1,3 +1,32 @@
 /*----------------
   //! Strings
 ------------------*/
+
+//? Concatenación
+
+let myName = "Ivan";
+let greeting = "Hola, " + myName + "!";
+console.log(greeting); // Imprime: Hola, Ivan!.
+console.log(typeof greeting); // Imprime: String.
+
+//? Longitud 
+
+console.log(greeting.length); // Imprime: 11 carácteres.
+
+//? Acceso a carácteres
+
+console.log(greeting[0]); // Imprime: H
+console.log(greeting[1]); // Imprime: o
+console.log(greeting[10]); // Imprime: !
+
+//? Métodos comunes
+
+console.log(greeting.toUpperCase()); // Cambia a mayúscula el texto.
+console.log(greeting.toLowerCase()); // Cambia a minúscula el texto.
+console.log(greeting.indexOf("Ivan")); // Indice donde se encuentra la palabra "Ivan", indice 6.
+console.log(greeting.includes("Hola")); // Valida si está incluida la cadena de texto "Hola".
+console.log(greeting.slice(0, 7)); // Toma la posición seleccionada.  
+console.log(greeting.replace("Ivan", "Ismael")); // Reemplaza el string por otro.  
+console.log(greeting); // 
+console.log(greeting); // 
+
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Creo este repositorio para ir documentando mi proceso de aprendizaje de JavaScri
 
 - 23-01-2025: Inicio subiendo lo realizado al nuevo repo en GitHub. 
 - 22-03-2025: Vuelvo a retormar despues de dos mese, que verguenza, :(
-- 25'03'2025: Quede por tema Strings 1:50:22
+- 25-03-2025: Quede por tema Strings 1:57:22
