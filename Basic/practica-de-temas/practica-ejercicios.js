fe77ef8e64b3962663457f48d0e0badc604ca2fc
Ivan Dario Madrid Daza <tecno85@gmail.com>
2026-03-26 11:34:33 -0500

    feat: practice password validation using do...while loop

--- Basic/practica-de-temas/practica-ejercicios.js
+++ Basic/practica-de-temas/practica-ejercicios.js
@@ -1,2 +1,17 @@
 // Ejercicio 4: Validación de contraseña
 // Simula un programa que pida una contraseña hasta que sea correcta.
+
+//Exercise 4: Password validation
+//Simulates a program that asks for a password until it is correct.
+
+const password = "1234";
+let userPassword;
+
+do {
+  userPassword = prompt('Enter the password: ');
+  if (userPassword !== password) {
+    console.log('Incorrect password, please try again.');
+  }
+} while (userPassword !== password);
+
+console.log('Correct pasword');
